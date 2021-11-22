3bc2fece65d0c0f5b5a63cfce20c60fd5d324e67
AdamChaplin23 <90810781+AdamChaplin23@users.noreply.github.com>
2021-11-22 15:09:18 -0600

    Add files via upload

---
+++ userpop.sql
@@ -0,0 +1,4 @@
+INSERT into SourNotes.users values (3, 'nirajjj', 'niraj123!', 'niraj@gmail.com', 'Niraj', 'Patel', 'user');
+INSERT into SourNotes.users values (4, 'ggorginal', 'gill123!', 'gg@gmail.com', 'Gurkirat', 'Gill', 'user');
+INSERT into SourNotes.users values (1, 'adamchap', 'adam123!', 'adam@gmail.com', 'Adam', 'Chaplin', 'user');
+INSERT into SourNotes.users values (2, 'uwemEkong', 'uwem123!', 'uwem@gmail.com', 'Uwem', 'Ekong', 'user');
