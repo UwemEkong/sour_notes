2d236fddd7e4b09ca834627803118f8d21ceea54
GGILL <ggill@cosmodigital.com>
2021-12-01 12:39:58 -0600

    Update SourPopulation.sql

--- SourPopulation.sql
+++ SourPopulation.sql
@@ -1,11 +1,11 @@
-INSERT INTO SourNotes.Album VALUES (1, "https://api.deezer.com/search?q=donda", 1);
+INSERT into sournotes.music VALUES (1, "https://api.deezer.com/track/1109731", true, "Lose Yourself", "Eminem", 3);
+INSERT into sournotes.music VALUES (2, "https://api.deezer.com/track/655877592", true, "3005", "Childish Gambino",5);
+INSERT into sournotes.music VALUES (3, "https://api.deezer.com/track/44812172", true,"Never Gonna Give You Up", "Rick Astley", 1);
+INSERT into sournotes.music VALUES (4, "https://api.deezer.com/track/7667065", true,"Runaway", "Kanye West", 5);
+INSERT into sournotes.music VALUES (5, "https://api.deezer.com/track/482883032", true, "Baby Shark", "Foozlebots", 2);
+INSERT into sournotes.music VALUES (6, "https://api.deezer.com/search?q=donda", false, "Donda", "Kanye West", 5);
 
-INSERT into SourNotes.Song VALUES (1, "https://api.deezer.com/track/1109731","Lose Yourself", "Eminem", 3);
-INSERT into SourNotes.Song VALUES (2, "https://api.deezer.com/track/655877592", "3005", "Childish Gambino",5);
-INSERT into SourNotes.Song VALUES (3, "https://api.deezer.com/track/44812172","Never Gonna Give You Up", "Rick Astley", 1);
-INSERT into SourNotes.Song VALUES (4, "https://api.deezer.com/track/7667065","Runaway", "Kanye West", 5);
-INSERT into SourNotes.Song VALUES (5, "https://api.deezer.com/track/482883032", "Baby Shark", "Foozlebots", 2);
-
-INSERT into SourNotes.Review VALUES (1, 1, "Lit", 5, 1, 1, 2);
-INSERT into SourNotes.Review VALUES (2, 2, "So good I cant even", 5, 2, 2, 3);
-INSERT into SourNotes.Review VALUES (3, 1, "Free iphone give away - send credit card number to urrealhomie@aol.com", 3, 3, 3, 1);
+INSERT into sournotes.review VALUES (1, 1, "Lit", 5, 1, 1);
+INSERT into sournotes.review VALUES (2, 2, "So good I cant even", 5, 2, 2);
+INSERT into sournotes.review VALUES (3, 1, "Free iphone give away - send credit card number to urrealhomie@aol.com", 3, 3, 3);
+INSERT into sournotes.review VALUES (4, 1, "Two thumbs up for this album", 5, 6, 3);
