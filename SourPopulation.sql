fa6bcd7e029c418f20ad4445005f0bee23052928
GGILL <ggill@cosmodigital.com>
2021-12-04 13:39:55 -0600

    show cover art for songs and albums

--- SourPopulation.sql
+++ SourPopulation.sql
@@ -1,11 +1,11 @@
 INSERT into sournotes.music VALUES (1, "https://api.deezer.com/track/1109731", true, "Lose Yourself", "Eminem", 3);
 INSERT into sournotes.music VALUES (2, "https://api.deezer.com/track/655877592", true, "3005", "Childish Gambino",5);
-INSERT into sournotes.music VALUES (3, "https://api.deezer.com/track/44812172", true,"Never Gonna Give You Up", "Rick Astley", 1);
+INSERT into sournotes.music VALUES (3, "https://api.deezer.com/track/781592622", true,"Never Gonna Give You Up", "Rick Astley", 1);
 INSERT into sournotes.music VALUES (4, "https://api.deezer.com/track/7667065", true,"Runaway", "Kanye West", 5);
 INSERT into sournotes.music VALUES (5, "https://api.deezer.com/track/482883032", true, "Baby Shark", "Foozlebots", 2);
-INSERT into sournotes.music VALUES (6, "https://api.deezer.com/search?q=donda", false, "Donda", "Kanye West", 5);
+INSERT into sournotes.music VALUES (6, "https://api.deezer.com/album/261749392", false, "Donda", "Kanye West", 5);
 INSERT into sournotes.music VALUES (7, "https://api.deezer.com/album/213413752", false, "Above All", "Jassa Dhillon", 5);
-INSERT into sournotes.music VALUES (8, "https://api.deezer.com/search?q='moonchild era'", false, "MoonChild Era", "Diljit Dosanjh", 5);
+INSERT into sournotes.music VALUES (8, "https://api.deezer.com/album/251148252'", false, "MoonChild Era", "Diljit Dosanjh", 5);
 
 INSERT into sournotes.review VALUES (1, 1, "Lit", 5, 1, 1);
 INSERT into sournotes.review VALUES (2, 2, "So good I cant even", 5, 2, 2);
