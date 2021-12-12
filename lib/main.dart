6ff9924c4a381cf61e212514372b803b23ca6b5d
AdamChaplin23 <b2482993@ben.edu>
2021-12-12 16:11:59 -0600

    splash shiteeee

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:sour_notes/pages/home_page.dart';
 import 'package:sour_notes/widgets/app.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +12,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: App());
+    return MaterialApp(
+      home: SplashScreen(
+          seconds: 5,
+          navigateAfterSeconds: HomePage(),
+          title: new Text(
+            'Welcome to SourNotes, share your thoughts and feelings about music here!',
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 35.0,
+                color: Colors.deepOrangeAccent),
+          ),
+          image: new Image.asset('assets/orange-music-note-hi.png'),
+          photoSize: 100.0,
+          backgroundColor: Color(0xFF303030),
+          styleTextUnderTheLoader: new TextStyle(),
+          loaderColor: Colors.deepOrangeAccent),
+    );
   }
 }
